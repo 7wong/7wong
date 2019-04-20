0dc83aa6b079cf318c3ea3465e5991a3d959a8da
7wong <wangyude77@gmail.com>
2019-04-20 22:38:01 +0800

    ...

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,125 +1,25 @@
-const sidebar = {
-    '/WEB/': [{
-            title: 'Vue',
-            children: [
-                'Vue/blog'
-            ]
-        },
+const pluginConf = require('../../config/pluginConf.js');
+const navConf = require('../../config/navConf.js');
+const sidebarConf = require('../../config/sidebarConf/index.js');
+const headConf = require('../../config/headConf.js');
 
-        {
-            title: 'React',
-            children: [
-                'React/react-router'
-            ]
-        }
-    ],
-
-   '/Life/': [{
-           title: 'Movies',
-           children: [
-               'Movies/blog'
-           ]
-       },
-
-       {
-           title: 'Music',
-           children: [
-               'Music/shop'
-           ]
-       }
-   ],
-
-    '/About/': [
-        {
-            title: 'About',
-            children: [
-                'About/readmea'
-            ]
-        },
-        {
-            title: 'Other',
-            children: [
-                'Other/readmeo'
-            ]
-        }
-    ]
-
-
-}
-
-
-
-const nav = [
-    {
-        text: 'Works',
-        items: [
-            { text: 'Vue', link: '/WEB/' + sidebar['/WEB/'][0]['children'][0] },
-            { text: 'React', link: '/WEB/' + sidebar['/WEB/'][1]['children'][0] }
-        ]
-    },
-    {
-        text: 'Life',
-        items: [
-            { text: 'Movies', link: '/Life/' + sidebar['/Life/'][0]['children'][0] },
-            { text: 'Music', link: '/Life/' + sidebar['/Life/'][1]['children'][0] }
-        ]
-    },
-    {
-        text: "About",
-        items: [
-            { text: 'About',link: '/About/' + sidebar['/About/'][0]['children'][0] },
-            { text: 'Other', link: '/About/' + sidebar['/About/'][1]['children'][0] },
-        ]
+module.exports = {
+  title: '飞跃高山与大洋的鱼',
+  description: '飞跃高山与大洋的鱼的文档, vuepress 文档',
+  locales: {
+    '/': {
+      lang: 'zh-CN', 
     }
-
-
-
-
-]
-
-
-
-
-var config = {
-
-    // 静态网站部署的目录
-    base: '/',
-
-    // 网站标题
-    title: '德先生的乐',
-    head: [
-        ['link', { rel: 'icon', href: '/images/favicon.ico' }]
-    ],
-
-
-    // <meta name="description" content="...">
-    description: '无可寄托者，用心躁也.',
-
-    markdown: {
-
-        // 显示代码行号
-        lineNumbers: true
-    },
-    themeConfig: {
-        // 项目的 github 地址
-        repo: '7wong/7wong',
-
-        // github 地址的链接名
-        repoLabel: 'GitHub',
-
-        // 当前 markdown 的 github 代码链接
-        //editLinks: true,
-
-        // 链接显示的文本
-        //editLinkText: '查看原文|编辑此页',
-        // 配置导航栏
-        nav,
-        sidebar,
-    }
-
-
-
+  },
+  head: headConf,
+  plugins: pluginConf,
+  themeConfig: {
+    lastUpdated: '上次更新',
+    repo: 'shanyuhai123/documents',
+    editLinks: true,
+    editLinkText: '编辑文档！',
+    docsDir: 'docs',
+    nav: navConf,
+    sidebar: sidebarConf
+  },
 }
-
-
-module.exports = config
